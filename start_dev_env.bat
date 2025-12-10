28fc1c3bb61fa51e9bbf922d92848f0610fe00f4
Adam Dasovich <adamdasovich@gmail.com>
2025-12-10 06:19:13 -0500

    changed the port

--- start_dev_env.bat
+++ start_dev_env.bat
@@ -5,6 +5,6 @@ start "Frontend Server" cmd /k "cd /d C:\Users\adamd\Project\claude2\frontend &&
 
 
 REM Start backend
-start "Backend Server" cmd /k "cd /d C:\Users\adamd\Project\claude2\backend && call venv\Scripts\activate && python manage.py runserver 8001"
+start "Backend Server" cmd /k "cd /d C:\Users\adamd\Project\claude2\backend && call venv\Scripts\activate && python manage.py runserver 8009"
 
 exit
